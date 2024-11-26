328ae2b9499ea6879a5bffb44aecb7a15f590dc7
jollygrin <JollyGrin@protonmail.com>
2024-11-26 19:06:04 +0100

    init

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
   // Required for GitHub Pages deployment - turn off when deploying to domain
-  basePath: process.env.NODE_ENV === "production" ? "/pwa" : "/",
+  basePath: process.env.NODE_ENV === "production" ? "/pwa" : "",
   images: {
     unoptimized: true,
   },
